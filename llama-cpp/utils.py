03abd4a0fe44726d74207ed3f625a58572302836
catsmile <catsmile.nico@gmail.com>
2023-09-25 23:34:33 +0900

    add docstring, add json category extractor

--- llama-cpp/utils.py
+++ llama-cpp/utils.py
@@ -1,4 +1,5 @@
-import os, tiktoken
+import os, re, tiktoken
+
 def write_csv_line(csv_path: str, values: dict):
     """Writes csv line(values) into csv (csv_path)
 
@@ -15,11 +16,54 @@ def write_csv_line(csv_path: str, values: dict):
         line = '","'.join([str(v).replace('"', "'") for v in values.values()])
         file.write(f'"{line}"\n')
 
-def count_tokens(text):
-    ENCODING = tiktoken.encoding_for_model("gpt-4")
-    return len(ENCODING.encode(text))
+def count_tokens(text, model="gpt-4"):
+    """Count number of tokens the text has
 
-def limit_tokens(text, max_tokens):
-    ENCODING = tiktoken.encoding_for_model("gpt-4")
-    tokens = ENCODING.encode(text)
-    return ENCODING.decode(tokens[:max_tokens])
+    Args:
+        text (str): Text to count
+        model (str, optional): Tokenizer model to use. Defaults to "gpt-4".
+
+    Returns:
+        int: The token count of the text
+    """
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(text))
+
+def limit_tokens(text, max_tokens, model="gpt-4"):
+    """Limits the text to meet max_token limit
+
+    Args:
+        text (str): Text to shrink
+        max_tokens (int): Maximum token allowed for text
+        model (str, optional): Tokenizer model to use. Defaults to "gpt-4".
+
+    Returns:
+        str: The text shrunk to the token limit
+    """
+    encoding = tiktoken.encoding_for_model(model)
+    tokens = encoding.encode(text)
+    return encoding.decode(tokens[:max_tokens])
+
+def extract_json_category(text):
+    """From a text, extract all the json string pair category and sub-category
+
+    Args:
+        text (str): String to be parsed
+
+    Returns:
+        list: list of json strings
+    """
+
+    # match the JSON string with dynamic values
+    pattern = r'{\s*"CATEGORY":\s*"(.*?)",\s*"SUB-CATEGORY":\s*\["(.*?)"\]\s*}'
+    matches = re.findall(pattern, text)
+
+    # Extract matched strings
+    json_strings = []
+    for match in matches:
+        category = match[0]
+        sub_category = match[1]
+        json_string = f'{{ "CATEGORY": "{category}", "SUB-CATEGORY": ["{sub_category}"] }}'
+        json_strings.append(json_string)
+
+    return json_strings
